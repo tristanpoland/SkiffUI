be5f13af8c132d00d6b6236acb5f2a818fcc5a33
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2022-11-02 00:02:30 -0400

    Added more info to services.sh startup command

--- app/services.sh
+++ app/services.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 #Start Docker in background
+echo '-----------------------------------'
+echo '| Starting internal docker damon...'
+echo '-----------------------------------'
 dockerd &
+echo '-------'
+echo '| Done'
+echo '-------'
+
 
 #Start Code Server in background
+echo '----------------------------------------------------------------'
+echo '| Starting code server in background for container management...'
+echo '----------------------------------------------------------------'
+
 exec \
     s6-setuidgid root \
         /app/code-server/bin/code-server \
@@ -13,9 +24,30 @@ exec \
             --disable-telemetry \
             --auth "password" &
 
+echo '-------'
+echo '| Done'
+echo '-------'
+
 #Start Flask backend
+echo '---------------------------'
+echo '| Starting SkiffUI damon...'
+echo '---------------------------'
+
 export FLASK_APP=/app/public/backend/app.py
 flask run &
 
+echo '-------'
+echo '| Done'
+echo '-------'
+
+
 #Start npm web server in foreground
+echo '----------------------------------'
+echo '| Starting front end web server...'
+echo '----------------------------------'
+
 npm start --host 0.0.0.0 --port 1027 --disable-host-check
+
+echo '-------'
+echo '| Done'
+echo '-------'
