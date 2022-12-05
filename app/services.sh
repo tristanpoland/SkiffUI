91dde687680e5518170d4ef89d855a3c7c13f46a
Tristan Poland <redstonecrafter126@gmail.com>
2022-12-05 13:40:53 -0500

    Removed checks (were throwing syntax errors)

--- app/services.sh
+++ app/services.sh
@@ -6,19 +6,6 @@ echo '| Starting internal docker damon...'
 echo '-----------------------------------'
 dockerd &
 
-retval = $ ? 
-if $retval == 0;
-then 
-    echo '-------' 
-    echo '| Done' 
-    echo '-------' 
-    continue 
-else;
-    echo "Failed to start Docker damon, please check docker logs" 
-    break 
-fi 
-
-
 #Start Code Server in background
 echo '----------------------------------------------------------------'
 echo '| Starting code server in background for container management...'
@@ -34,18 +21,6 @@ exec \
             --auth "password" &
 
 
-retval = $ ? 
-if $retval == 0;
-then 
-    echo '-------' 
-    echo '| Done' 
-    echo '-------' 
-    continue 
-else;
-    echo "Failed to start code server, please check container logs" 
-    break 
-fi 
-
 #Start Flask backend
 echo '---------------------------'
 echo '| Starting SkiffUI damon...'
@@ -54,18 +29,6 @@ echo '---------------------------'
 export FLASK_APP=/app/public/backend/app.py
 flask run &
 
-retval = $ ? 
-if $retval == 0;
-then 
-    echo '-------' 
-    echo '| Done' 
-    echo '-------' 
-    continue 
-else;
-    echo "Failed to start SkiffUI damon, please check SkiffUI logs" 
-    break 
-fi 
-
 
 #Start npm web server in foreground
 echo '----------------------------------'
@@ -73,16 +36,3 @@ echo '| Starting front end web server...'
 echo '----------------------------------'
 
 npm start --host 0.0.0.0 --port 1027 --disable-host-check
-
-retval = $ ? 
- 
-if $retval == 0;
-then 
-    echo '-------' 
-    echo '| Done' 
-    echo '-------' 
-    continue 
-else;
-    echo "Failed to start web server, please check container logs." 
-    break 
-fi 
