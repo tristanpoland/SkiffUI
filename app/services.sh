171d152640ad71396fedfcb8965b7c58354a8ea9
Tristan James Poland <redstonecrafter126@gmail.com>
2022-12-18 03:02:27 +0000

    Fixed local websocket for SkiffUI (Public version still broken)

--- app/services.sh
+++ app/services.sh
@@ -4,21 +4,21 @@
 echo '-----------------------------------'
 echo '| Starting internal docker damon...'
 echo '-----------------------------------'
-dockerd &
+#dockerd &
 
 #Start Code Server in background
 echo '----------------------------------------------------------------'
 echo '| Starting code server in background for container management...'
 echo '----------------------------------------------------------------'
 
-exec \
-    s6-setuidgid root \
-        /app/code-server/bin/code-server 
-            --bind-addr 0.0.0.0:8443 \
-            --user-data-dir /.config/data \
-            --extensions-dir /.config/extensions \
-            --disable-telemetry \
-            --auth "password" &
+#exec \
+#    s6-setuidgid root \
+#        /app/code-server/bin/code-server 
+#            --bind-addr 0.0.0.0:8443 \
+#            --user-data-dir /.config/data \
+#            --extensions-dir /.config/extensions \
+#            --disable-telemetry \
+#            --auth "password" &
 
 
 #Start Flask backend
@@ -27,7 +27,7 @@ echo '| Starting SkiffUI damon...'
 echo '---------------------------'
 
 export FLASK_APP=/app/public/backend/app.py
-flask run &
+pythonw -m flask run -h 0.0.0.0 -p 1027 > log.txt 2>&1 &
 
 
 #Start npm web server in foreground
