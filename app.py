efc382e7ea86d01d91062bbb665ae4e5085d2843
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2023-03-02 14:34:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from nodezator import Node, Connection, NodeZator
 
 app = Flask(__name__)
