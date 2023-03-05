a45d4988d229c3c552f1f72a71200cf32a580b75
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2023-03-05 14:59:50 -0500

    Finalized setup scripts

--- setup.bat
+++ setup.bat
@@ -1,1 +1,4 @@
-pip3.8.exe install '..\OpenImageIO-2.1.18.1-cp38-cp38-win_amd64.whl'
+cd .\src
+pip3.8.exe install -r ..\requirements.txt
+pip3.8.exe install ..\OpenImageIO-2.1.18.1-cp38-cp38-win_amd64.whl
+python3.8.exe .\main.py
