a2c31a23a876e4347cd547906d30076ed37bbf07
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2023-03-11 18:31:59 -0500

    Fixed infinite recursion error in the docker state engine

--- src/main.py
+++ src/main.py
@@ -14,6 +14,22 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------
+# SkiffUI Copyright 2020-2023 by Gameplex Software and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+
 import os
 import sys
 import platform
@@ -50,7 +66,7 @@ import builtins
 builtins.__dict__['_'] = wx.GetTranslation
 
 # Start docker state scanning
-state_tracker.build_docker_state()
+state_tracker
 
 class MainApp(wx.App):
 
--- src/nodes/state_tracker.py
+++ src/nodes/state_tracker.py
@@ -1,24 +1,66 @@
-import threading
-import time
 import json
 import docker
+import time
+import threading
+import uuid
 
-def build_docker_state():
-    print('[Debbug] Got docker state')
+def generate_node_id():
+    return str(uuid.uuid4())
+
+def get_node_data():
+    print("Started generating Docker state")
     client = docker.from_env()
-    nodes = []
-    for container in client.containers.list():
-        node = {"name": container.name, "id": container.id}
-        nodes.append(node)
-    json_data = {"nodes": nodes}
-    with open("gimelstudio.skf", "w") as f:
-        json.dump(json_data, f)
-        print('[Debug] Built Docker state map')
+    containers = client.containers.list()
+    nodes = {}
+    for container in containers:
+        node_id = generate_node_id()
+        node = {
+            "idname": "DockerNode",
+            "pos": [
+                0,
+                0
+            ],
+            "muted": False,
+            "selected": False,
+            "active": False,
+            "expanded": False,
+            "parameters": {},
+            "properties": {
+                "name": {
+                    "idname": "name",
+                    "value": container.name
+                },
+                "status": {
+                    "idname": "status",
+                    "value": container.status
+                }
+            }
+        }
+        nodes[node_id] = node
 
-    # Rescan Docker every 2 seconds
-    threading.Timer(2.0, build_docker_state).start()
+    node_graph = {
+        "nodes": nodes
+    }
 
-if __name__ == "__main__":
-    # Run on a separate thread
-    t = threading.Thread(target=build_docker_state)
-    t.start()
+    data = {
+        "application": "SkiffUI",
+        "file_type": "SkiffUI Network Map",
+        "app_version": "0.0.1",
+        "app_version_tag": " pre-alpha 1",
+        "app_version_full": "0.0.1 pre-alpha 1",
+        "node_graph": node_graph
+    }
+
+    return data
+
+def write_json():
+    data = get_node_data()
+    formatted_json = json.dumps(data, indent=4)
+    with open('docker_state.skf', 'w') as f:
+        f.write(formatted_json)
+        print("Docker state built")
+
+    # Schedule the function to run again after 2 seconds
+    threading.Timer(2.0, write_json).start()
+
+write_json()
