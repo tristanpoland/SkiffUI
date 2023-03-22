aa0dd74ebb35ba1715071b9b01eddaf40888e82d
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2023-03-22 13:14:41 -0400

    Added printing of node UUIDs to console
    This was done for debugging to prep for an attempt to instance out the nodes

--- src/gsnodegraph/gsnodegraph/graph/base.py
+++ src/gsnodegraph/gsnodegraph/graph/base.py
@@ -14,6 +14,22 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------
+# SkiffUI Copyright 2020-2023 by Gameplex Software and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+
 import uuid
 import wx
 import wx.lib.agw.flatmenu as flatmenu
@@ -44,6 +60,7 @@ ID_CONTEXTMENU_SELECTALLNODES = wx.NewIdRef()
 
 class NodeGraph(wx.ScrolledCanvas):
     def __init__(self, parent, registry, *args, **kwds):
+        print("Hello from GSNodeGraph")
         self.parent = parent
         self.node_registry = registry
 
@@ -683,6 +700,7 @@ class NodeGraph(wx.ScrolledCanvas):
             node_id = uuid.uuid4().hex
         else:
             node_id = nodeid
+        print(node_id, "Was added to the graph")
         node = self.node_registry[idname](self, node_id)
         node.Init(idname)
         self.nodes[node.id] = node
