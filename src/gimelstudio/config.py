2b35b5896442ae4d466fc517a1a236ea946143ab
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2023-08-29 17:43:23 -0400

    Updated copyrights

--- src/gimelstudio/config.py
+++ src/gimelstudio/config.py
@@ -14,6 +14,22 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------
+# SkiffUI Copyright 2020-2023 by Gameplex Software and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+
 import os
 import json
 import os.path
@@ -79,7 +95,7 @@ class AppConfiguration(AppData):
                     try:
                         f.write(default_config)
                     except:
-                        print("[WARN] Error writing config file defaults,. Here be dragons")
+                        print("[WARN] Error writing config file defaults, this will likely cause future errors. Here be dragons")
 
             with open(self.app_config_file, "r") as file:
                 self.prefs = json.load(file)
